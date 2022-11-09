13e04288a3813e03e1fe6c92a59d19fa8578b298
Anupam <koundalankush7@gmail.com>
2022-11-10 00:36:35 +0530

    vpc added

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ module "vpc" {
     vpc_cidr = "10.0.0.0/16"
 }
 
+/*
 module "circleci_context" {
     source = "./modules/circle-ci"
     circleci_context_name = "circleci_context"
@@ -11,3 +12,5 @@ module "circleci_context" {
        circleci = circleci.ci
   }
 }
+
+*/
