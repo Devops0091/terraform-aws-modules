0201606237413cf73c3540dd0e751af29d9a1878
Devops <devops@nomail.com>
2022-11-10 16:25:26 +0530

    code added

--- provider.tf
+++ provider.tf
@@ -10,7 +10,6 @@ provider "aws" {
 */
 provider "circleci" {
   # Configuration options
-  alias = "ci"
   api_token = "8daab594c8bb58e6e3aee9c2d0909fbf77972037"
   vcs_type     = "github"
   organization = "ankush0991"
