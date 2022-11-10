fc591cdddaf3d961943931f14d29ae4305573286
Devops <devops@nomail.com>
2022-11-10 15:54:07 +0530

    code added

--- modules/circle-ci/main.tf
+++ modules/circle-ci/main.tf
@@ -12,3 +12,11 @@ variable "circleci_context_name" {
     #default = "sdcsc"
 }
 
+
+provider "circleci" {
+  # Configuration options
+  alias = "ci"
+  api_token = "8daab594c8bb58e6e3aee9c2d0909fbf77972037"
+  vcs_type     = "github"
+  organization = "ankush0991"
+  }
