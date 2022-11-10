5c64b0541992eb7b3676464b55313474fe360ac4
Devops <devops@nomail.com>
2022-11-10 16:08:47 +0530

    code added

--- main.tf
+++ main.tf
@@ -7,6 +7,12 @@ module "vpc" {
 module "context" {
     source = "./modules/circle-ci"
     circleci_context_name = "circleci_context"
-
+provider "circleci" {
+  # Configuration options
+  alias = "ci"
+  api_token = "8daab594c8bb58e6e3aee9c2d0909fbf77972037"
+  vcs_type     = "github"
+  organization = "ankush0991"
+  }
 }
 
