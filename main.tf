19485531c9061cb59243b8967c24f25c9a1cca2d
Devops <devops@nomail.com>
2022-11-10 13:22:00 +0530

    code added

--- main.tf
+++ main.tf
@@ -7,10 +7,24 @@ module "vpc" {
 module "circleci_context" {
     source = "./modules/circle-ci"
     circleci_context_name = "circleci_context"
- 
+
     providers = {
        circleci = circleci.ci
   }
 }
 
 */
+
+resource "circleci_context" "build" {
+  name      = var.circleci_context_name
+   provider = circleci.ci
+
+}
+
+
+
+variable "circleci_context_name" {
+    type = string
+    description = "circle ci context name"
+    default = "sdcsc"
+}
