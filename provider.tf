29385beca8ca291d27197efd1b6dfd458515d28c
ankush0991 <75529170+ankush0991@users.noreply.github.com>
2022-11-30 16:30:48 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,9 +3,9 @@
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-   assume_role {
-    role_arn = "arn:aws:iam::915872208696:role/OrganizationAccountAccessRole"
-  }
+  # assume_role {
+  #  role_arn = "arn:aws:iam::915872208696:role/OrganizationAccountAccessRole"
+  #}
 }
 /*
 provider "circleci" {
