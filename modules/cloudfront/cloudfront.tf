00aa4a9d06ab0cf32003bf38e38811816d255889
ankush0991 <75529170+ankush0991@users.noreply.github.com>
2022-11-30 16:46:52 +0530

    Update cloudfront.tf

--- modules/cloudfront/cloudfront.tf
+++ modules/cloudfront/cloudfront.tf
@@ -17,7 +17,7 @@ module "cdn" {
   origin = {
       domain_name = "${module.s3_one.s3_bucket_website_endpoint}"
        origin_id   = "cloudfront"
-     custom_origin_config {
+     custom_origin_config = {
       http_port              = "80"
       https_port             = "443"
       origin_protocol_policy = "http-only"
